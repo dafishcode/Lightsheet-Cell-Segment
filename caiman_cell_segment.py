bb9b9eac627b7451fd7c23f851afd56c67a28286
Richard Rosch <roschkoenig@gmail.com>
2018-11-21 12:21:19 +0000

    Committed some more

--- caiman_cell_segment.py
+++ caiman_cell_segment.py
@@ -7,3 +7,10 @@ from scipy.ndimage.filters import gaussian_filter
 import sys
 
 import caiman as cm
+from caiman.utils.visualization import nb_view_patches3d
+import caiman.source_extraction.cnmf as cnmf
+from caiman.components_evaluation import evaluate_components, estimate_components_quality_auto
+from caiman.cluster import setup_cluster
+from caiman,oaths import caiman_datadir
+
+import bokeh.plotting as bpl
