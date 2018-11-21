ec6ef29da3086beaae334d1d681c9bdbee04e30b
Richard Rosch <roschkoenig@gmail.com>
2018-11-21 13:19:33 +0000

    Tif locator code

--- caiman_cell_segment.py
+++ caiman_cell_segment.py
@@ -10,6 +10,7 @@ import os
 import psutil
 from scipy.ndimage.filters import gaussian_filter
 import sys
+import re
 
 import caiman as cm
 from caiman.utils.visualization import nb_view_patches3d
@@ -20,9 +21,33 @@ from caiman.paths import caiman_datadir
 
 import bokeh.plotting as bpl
 
+# Housekeeping
+#===============================================================================
+# Define folder locations
+#-------------------------------------------------------------------------------
+Fbase  = '/Volumes/GoogleDrive/My Drive/Research/1809 Seizure criticality/data/Zebrafish/ZFRR007-01'
+Fmvct  = Fbase + os.sep + 'Realigned'
+Fshort = Fmvct + os.sep + 'Test'
+
+# Load tifs
+#-------------------------------------------------------------------------------
+Fthis   = Fshort
+tifs    = []
+ftifs   = os.listdir(Fthis)
+r       = re.compile('^.*[tif|tiff]$')
+ftifs   = list(filter(r.match, ftifs))
+for ff in ftifs: tifs.append(os.path.join(Fthis, ff))
+print("I found " + str(len(tifs)) + " tif files")
+
+# Segmentation parameter settings
+#===============================================================================
 # Set up parallel processing
 #-------------------------------------------------------------------------------
-n_processes = psutil.cpu_count
+n_processes = psutil.cpu_count()
+print('using ' + str(n_processes) + ' processes')
+print('Stopping cluster to avoid unnecessary use of memory')
+sys.stdout.flush()
+cm.stop_server()
 
 # Set Parameters (taken from standard demo)
 #-------------------------------------------------------------------------------
